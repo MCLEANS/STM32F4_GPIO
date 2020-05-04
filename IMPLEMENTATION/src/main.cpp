fa9546fbc60a14673f5c55e7ad420d42a116095b
MCLEANS <jackagwaya@gmail.com>
2020-05-04 21:17:25 +0300

    GPIO procedure

--- IMPLEMENTATION/src/main.cpp
+++ IMPLEMENTATION/src/main.cpp
@@ -14,7 +14,15 @@
 
 int main(void)
 {
-
+	/*
+	 * Set pin to output
+	 * 1. Enable GPIO RCC
+	 * 2. Set mode to either input or output
+	 * 3. Set output type( Push-pull or open drain)
+	 * 4. output speed
+	 * 5. Pull up or pull down
+	 *
+	 */
 	while(1){
 
 	}

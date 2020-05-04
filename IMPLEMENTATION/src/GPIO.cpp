b21de1a94ef9f79c5d92de2dbed6bcb9946040b8
MCLEANS <jackagwaya@gmail.com>
2020-05-04 21:20:55 +0300

    GPIO source file

---
+++ IMPLEMENTATION/src/GPIO.cpp
@@ -0,0 +1,21 @@
+/*
+ * GPIO.cpp
+ *
+ *  Created on: May 4, 2020
+ *      Author: MCLEANS
+ */
+
+#include "GPIO.h"
+
+namespace custom_libraries {
+
+_GPIO::_GPIO() {
+	// TODO Auto-generated constructor stub
+
+}
+
+_GPIO::~_GPIO() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace custom_libraries */
